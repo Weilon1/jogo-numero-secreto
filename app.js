8774580a13ca6159cd6109571ab8c2175dc8d224
Weilon Barbosa Farias De Oliveira <weilon.farias@DASA.COM.BR>
2024-12-15 10:53:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+let listaNumerosSorteados = [];
+let numeroLimite = 10
+let numeroSecreto = 5//gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
+}
+
+function mensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+mensagemInicial()
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'Tentativas' : 'Tentativa';
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('h1', 'Acertou');
+        exibirTextoNaTela('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        // 'reiniciar' = id do elemento
+        // 'disabled' = Nome do atributo a ser removido
+    } else {
+
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O numero secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O numero secreto é maior');
+        }
+
+        tentativas ++;
+        limparCampo();
+    }
+
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementoNaLista = listaNumerosSorteados.length;
+
+    if (quantidadeDeElementoNaLista == numeroLimite) {
+        listaNumerosSorteados = [];
+    }
+
+    if (listaNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio() // Recurção chama a própria função
+    } else {
+        listaNumerosSorteados.push(numeroEscolhido);
+        console.log(listaNumerosSorteados);
+        return numeroEscolhido;
+    }
+};
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+};
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true)
+}
